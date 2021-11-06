9324721797a741f6159f6ac51fdf06e7f461e5bb
Michael Gulasy <michael.gulasy@gmail.com>
2021-11-06 16:53:57 -0400

    commit to main

--- scraper.py
+++ scraper.py
@@ -14,15 +14,22 @@ print(website)
 
 def getPrice(website):
     craigslist = requests.get(website)
-    # print(yahoo.text[0:500]) #Shows the first 500 words of the html of the site
-
-    #reads the html and makes sense of the structure
     s = BeautifulSoup(craigslist.text, 'html.parser') 
+    numOfListings = s.find('span', attrs={'class':'totalcount'})
+    count = int(numOfListings.text)
+    print(count)
+    page = 0
+    results = set()
     #finds all the span sections with the class attribute equal to what is listed
-    results = s.findAll('li', class_='result-row') 
-    sys.stdout = open('test.html', 'w')
-    print(len(results)) #prints how many span sections found that fit the description
-    #price = result.text
+    while count > 0:
+        craigslist = requests.get(website)
+        results = results.append(s.findAll('li', class_='result-row')) 
+        sys.stdout = open('wayne.html', 'w')
+        print(len(results)) 
+        count -= 120
+        page += 120
+        print(count)
+        website = 'https://' + str(city) + '.craigslist.org/d/cars-trucks/search/cta?s=' + str(page) 
     return results
 
 def printList(results):
